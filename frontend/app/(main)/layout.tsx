89d898c24052df1343711f8a15b5e995eacaf643
Vedant <48868398+vedantb2@users.noreply.github.com>
2025-12-03 14:26:38 +0000

    .

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -8,8 +8,8 @@ export default function MainLayout({
   return (
     <div className="min-h-screen bg-neutral-200 dark:bg-black">
       <Sidebar />
-      <main className="md:ml-[18%] min-h-screen p-3">
-        <div className="min-h-[calc(100vh-24px)] px-8 py-12 rounded-2xl bg-white dark:bg-neutral-900">
+      <main className="md:ml-[18%] min-h-screen p-3 flex">
+        <div className="flex-1 min-h-max px-8 py-12 rounded-2xl bg-white dark:bg-neutral-900">
           {children}
         </div>
       </main>
