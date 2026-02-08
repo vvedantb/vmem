3312703d8512cff57df3ebc413525301f560792e
Vedant <48868398+vedantb2@users.noreply.github.com>
2026-02-08 01:31:51 +0000

    convert to monorepo structure

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./archived/**/*.{js,ts,jsx,tsx,mdx}",
     "*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
